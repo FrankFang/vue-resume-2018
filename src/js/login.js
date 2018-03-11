30843d87fdb0db6add019d869aa1f495877b962c
Frank Fang <FrankFang1990@gmail.com>
2018-03-11 12:02:03 +0800

    路由化

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,5 @@
-Vue.component('login', {
+console.log(4)
+window.Login = {
   data () {
     return {
       login: {
@@ -28,7 +29,7 @@ Vue.component('login', {
   <div class="login" v-cloak>
     <form class="form" @submit.prevent="onLogin">
       <h2>登录</h2>
-      <button type="button" @click="$emit('close')">关闭</button>
+      <router-link to="/">关闭</router-link>
       <div class="row">
         <label>邮箱</label>
         <input type="text" v-model="login.email">
@@ -39,9 +40,11 @@ Vue.component('login', {
       </div>
       <div class="actions">
         <button type="submit">提交</button>
-        <a href="#" @click="onClickSignUp">注册</a>
+        <router-link to="/signUp">注册</router-link>
       </div>
     </form>
   </div>
   `
-})
+}
+Vue.component('login', window.Login)
+console.log(3)
